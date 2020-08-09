be151c0d0d0158d5a770053863fc06496e0a3886
mfkurt11 <68709611+mfkurt11@users.noreply.github.com>
2020-08-09 11:04:31 -0400

    Update FirstClass.java
    This is my the first change from remote master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("THis is the first changes from local master");
+		System.out.println("THis is the first changes from remote master");
 
 	}
 
